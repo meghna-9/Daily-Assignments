c40a8c965f65c6e1e69ea52cc1d7d2797cdafc4e
meghna-9 <meghnapandey2006@gmail.com>
2026-06-06 19:20:06 +0530

    Perfect no.

---
+++ day5a.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+
+    cout<<"Enter a number: ";
+    cin >> n;
+       
+       for(int i=1;i<=n/2;i++){
+        if(n%i==0){
+            sum=sum+i;
+        }
+       }
+       if(sum==n)
+          cout<< n << "is a Perfect no.";
+       else
+          cout<< n<<"is not a Perfect no.";
+    return 0;         
+}
