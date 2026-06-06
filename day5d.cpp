997804ad45f5f5bb06f2f81296fc8a69e7594992
meghna-9 <meghnapandey2006@gmail.com>
2026-06-06 19:21:47 +0530

    Largest prime factor of a no.

---
+++ day5d.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int largest = 1;
+
+    for (int i = 2; i <= n; i++) {
+        while (n % i == 0) {
+            largest = i;
+            n /= i;
+        }
+    }
+
+    cout << "Largest Prime Factor = " << largest;
+    return 0;
+}
